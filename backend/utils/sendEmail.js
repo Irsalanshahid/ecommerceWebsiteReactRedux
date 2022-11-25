2fa1a2b6343ce4f3b15e29696d4a609d6ecb8c40
Arslan Shahid <irsalan.uetian08@gmail.com>
2022-11-26 00:27:17 +0500

    corrections made

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,9 +1,7 @@
 const nodeMailer = require("nodeMailer");
 
 const sendEmail = async (options) => {
-  console.log(
-    process.env.SMTP_SERVICE + process.env.SMTP_MAIL + process.env.SMTP_PASSWORD
-  );
+  
   const transporter = nodeMailer.createTransport({
     service: process.env.SMTP_SERVICE,
     auth: {
